bea01cac2057d664e8c6413e4c9b22b771df8315
m1roSlavit <43876601+m1roSlavit@users.noreply.github.com>
2019-12-15 00:21:06 +0200

    123

---
+++ urkproj/#source/js/main.js
@@ -0,0 +1,4 @@
+//@prepros-append jquery-3.4.1.js
+//@prepros-append parallax.js
+//@prepros-append slick.js
+//@prepros-append script.js
---
+++ urkproj/#source/js/parallax-conf.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+	$(".wrapper").addClass("active")
+
+	var parallaxElem = document.getElementById("scene");
+	var parallaxInstance = new Parallax(parallaxElem);
+
+});
